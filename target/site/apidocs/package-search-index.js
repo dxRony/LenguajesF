5b688b738b2457caaf5e5ad7310b60a65ce1170d
dxRony <romaroag@gmail.com>
2023-08-22 12:47:29 -0600

    agregando jar

--- target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mycompany.parserpy"},{"l":"com.mycompany.parserpy.be"},{"l":"com.mycompany.parserpy.fe"}];updateSearchResults();
