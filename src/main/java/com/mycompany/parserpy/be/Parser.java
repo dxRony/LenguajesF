24b3e533146255e48a33849738aeb9ece8d8a905
dxRony <romaroag@gmail.com>
2023-09-26 13:01:54 -0600

    agregando parte de los if

--- src/main/java/com/mycompany/parserpy/be/Parser.java
+++ src/main/java/com/mycompany/parserpy/be/Parser.java
@@ -25,9 +25,12 @@ public class Parser {
             System.out.println(tokens.get(indice).getTipo1() + "/" + tokens.get(indice).getTipo2());
             if (tokens.get(indice).getTipo1().equals("Identificador")) {//se esta declarando un identificador              
                 asignacion();
+            } else if (tokens.get(indice).getLexema().equals("if")) {//se esta iniciando un if
+                bloqueIf();
             } else {
                 break;
             }
+
         }
 
     }
@@ -47,10 +50,10 @@ public class Parser {
                     if (tokens.get(indice).getLexema().equals(",")) {//se va a dar el otro valor
                         expresion();
                         indice++;
-                    } else{
+                    } else {
                         //error sintactico
                     }
-                } else  {
+                } else {
                     //error sintactico
                 }
             } else {
@@ -63,19 +66,23 @@ public class Parser {
 
     }
 
-    private void expresion() {
+    private boolean expresion() {
         indice++;
         if (tokens.get(indice).getTipo1().equals("Cadena")) {//se le da el tipo cadena
             System.out.println("hay asignacion de tipo: cadena");
+            return true;
 
         } else if (tokens.get(indice).getTipo1().equals("Entero")) {//se le da tipo entero
             System.out.println("hay asignacion de tipo: entero");
+            return true;
 
         } else if (tokens.get(indice).getTipo1().equals("Decimal")) {//se le da tipo decimal
             System.out.println("hay asignacion de tipo: decimal");
+            return true;
 
         } else if (tokens.get(indice).getTipo2().equals("Constante Booleana")) {//se le da tipo boolean
             System.out.println("hay asignacion de tipo: booleana");
+            return true;
 
         } else if (tokens.get(indice).getLexema().equals("[")) {//se abre un arreglo tipo=?
             System.out.println("se abre array");
@@ -100,9 +107,44 @@ public class Parser {
             } else {
                 //error sintactico
             }
+            return false;
         } else {
             System.out.println("no hay expresion");
             //error sintactico
         }
+        return false;
     }
+
+    private void bloqueIf() {
+        indice++;
+
+        if (expresion()) {//en el if hay una expresion
+            indice++;
+            if (tokens.get(indice).getLexema().equals(":")) {//aparecen los 2 puntos del if
+                indice++;
+                //#bloque de codigo
+            } else if (tokens.get(indice).getTipo2().equals("Comparacion")) {//hay un simbolo de comparacion
+                indice++;
+                if (expresion()) {//hay otra expresion
+                    indice++;
+                    if (tokens.get(indice).getLexema().equals(":")) {//2 puntos luego de la comparacion
+                        indice++;
+                        //#bloque de codigo
+                    } else {
+                        //error sintactico
+                    }
+                } else {
+                    //error sintactico
+                }
+            } else if (tokens.get(indice).getLexema().equals("else")) {//entrando a else
+                indice++;
+                //#bloque de codigo
+            } else {
+                //error sintactico
+            }
+        } else {
+            //error sintactico
+        }
+    }
+
 }
